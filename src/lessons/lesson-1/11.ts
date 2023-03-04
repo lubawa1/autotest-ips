e5f0381119df7caadc9409db5ffd457e971cea13
lubawa1 <maddycrazy996@gmail.com>
2023-03-04 20:03:27 +0300

    11, 12 задачи
    11, 12 задачи

--- src/lessons/lesson-1/11.ts
+++ src/lessons/lesson-1/11.ts
@@ -1,5 +1,10 @@
-const sum = function (sum) {
-    
-    console.log(sum)
+function calc(x: number, y: number, fn: (x: number, y: number) => number): number {
+    return fn(x, y)
 }
-const calc = function (a: number, b: number, callback)
+const sum = calc(3, 7, (x, y) => x + y)
+const diff = calc(3, 7, (x, y) => x - y)
+const mul = calc(3, 7, (x, y) => x * y)
+const div = calc(3, 7, (x, y) => x / y)
+const array = [sum, diff, mul, div]
+for (const result of array)
+    console.log(result)
---
+++ src/lessons/lesson-1/12.ts
@@ -0,0 +1,10 @@
+for (let i: number = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i)
+    }
+}
+for (let i: number = 0; i <= 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
+    }
+}
